5b3afb2fcc57622665c9a752789374503b81b80a
rioforce <rioforce.legend@hotmail.com>
2016-01-31 21:00:23 -0500

    Fixing navbar

---
+++ script.js
@@ -0,0 +1,19 @@
+(function(){
+  "use strict";
+  
+  var Qnav = document.querySelector(".navigation"),
+      QheaderContentImage = document.querySelector("#header-content-image");
+  function toggleFixedNav() {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset >= 298) {
+      Qnav.classList.add("fixed");
+      QheaderContentImage.classList.add("fixed");
+    } else {
+      Qnav.classList.remove("fixed");
+      QheaderContentImage.classList.remove("fixed");
+    }
+  }
+  
+  toggleFixedNav();
+  window.addEventListener("scroll", toggleFixedNav);
+}());
