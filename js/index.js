b7c46669f9df9544743e99f079ddb20c86975b0c
Caleb Ely <le717@users.noreply.github.com>
2021-02-01 20:28:08 -0500

    Go to client subpage on small screens

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
 import { load_page, on_site_load } from "./client-work.js";
 import { find_parent } from "./find-parent.js";
 
-// A film was possibly clicked
-document.querySelector(".featuredon").addEventListener("click", function (e) {
-  e.preventDefault();
-  // Find the container element for this film
-  let qFilm = find_parent(e.target, ".film");
+// Go to the client work subpage if we're on a smaller screen
+let is_small_screen = window.getComputedStyle(document.querySelector("body"), "::before").content;
+if (is_small_screen === "none") {
+  // A film was possibly clicked
+  document.querySelector(".featuredon").addEventListener("click", function (e) {
+    e.preventDefault();
+    // Find the container element for this film
+    let qFilm = find_parent(e.target, ".film");
 
-  // Nope, no film was clicked
-  if (qFilm === null) {
-    return false;
-  }
-
-  // Load the film
-  load_page(qFilm);
-});
+    // Nope, no film was clicked
+    if (qFilm === null) {
+      return false;
+    }
+    load_page(qFilm);
+  });
+}
 
 window.addEventListener("DOMContentLoaded", function () {
-  let film = null;
   // The user is loading a specific film,
   // get the hash and attempt to load it
+  let film = null;
   if (window.location.hash !== "") {
     film = window.location.hash.match(/#([a-z-]*?)$/i)[1].toLowerCase();
   }
